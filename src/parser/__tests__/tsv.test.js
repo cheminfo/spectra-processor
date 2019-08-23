1d7b5fb62f897b7cb8eb58459b41217e6b226827
Luc Patiny <luc@patiny.com>
2019-08-23 14:52:34 +0200

    Working on TSV

--- src/parser/__tests__/tsv.test.js
+++ src/parser/__tests__/tsv.test.js
@@ -5,7 +5,6 @@ import tsvParser from '../tsv';
 
 test('tsvParser', () => {
   const text = readFileSync(join(__dirname, 'data.tsv'), 'utf8');
-  const data = tsvParser(text);
-
-  console.log(data);
+  tsvParser(text);
+  expect(true).toBe(true);
 });
