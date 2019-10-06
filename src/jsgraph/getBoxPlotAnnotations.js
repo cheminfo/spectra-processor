6ba0c25689e5c5b7e44cf61f6914398084c42bf8
Luc Patiny <luc@patiny.com>
2019-10-06 07:34:59 +0200

    feat: allow to select what is displayed in getBoxPlotAnnotations

--- src/jsgraph/getBoxPlotAnnotations.js
+++ src/jsgraph/getBoxPlotAnnotations.js
@@ -12,81 +12,86 @@ export function getBoxPlotAnnotations(spectra, options = {}) {
   let annotations = [];
 
   let data = getBoxPlotData(spectra);
-  let q13 = [];
-  for (let i = 0; i < data.x.length; i++) {
-    q13.push({
-      x: data.x[i],
-      y: data.Q1[i]
-    });
-  }
-  for (let i = data.x.length - 1; i >= 0; i--) {
-    q13.push({
-      x: data.x[i],
-      y: data.Q3[i]
-    });
-  }
-
-  annotations.push({
-    type: 'polygon',
-    layer: 0,
-    properties: {
-      fillColor: q13FillColor,
-      fillOpacity: q13FillOpacity,
-      strokeWidth: 0.0000001,
-      position: q13
+  if (q13FillOpacity && q13FillColor) {
+    let q13 = [];
+    for (let i = 0; i < data.x.length; i++) {
+      q13.push({
+        x: data.x[i],
+        y: data.Q1[i]
+      });
     }
-  });
-
-  let q2 = [];
-  for (let i = 0; i < data.x.length; i++) {
-    q2.push({
-      x: data.x[i],
-      y: data.Q2[i]
-    });
-  }
-  // Temp code because polyline is not working
-  for (let i = data.x.length - 1; i >= 0; i--) {
-    q2.push({
-      x: data.x[i],
-      y: data.Q2[i]
-    });
-  }
-
-  annotations.push({
-    type: 'polygon',
-    layer: 0,
-    properties: {
-      strokeWidth: q2StrokeWidth,
-      strokeColor: q2StrokeColor,
-      position: q2
+    for (let i = data.x.length - 1; i >= 0; i--) {
+      q13.push({
+        x: data.x[i],
+        y: data.Q3[i]
+      });
     }
-  });
-
-  let minMax = [];
-  for (let i = 0; i < data.x.length; i++) {
-    minMax.push({
-      x: data.x[i],
-      y: data.min[i]
-    });
-  }
-  for (let i = data.x.length - 1; i >= 0; i--) {
-    minMax.push({
-      x: data.x[i],
-      y: data.max[i]
+    annotations.push({
+      type: 'polygon',
+      layer: 0,
+      properties: {
+        fillColor: q13FillColor,
+        fillOpacity: q13FillOpacity,
+        strokeWidth: 0.0000001,
+        position: q13
+      }
     });
   }
 
-  annotations.push({
-    type: 'polygon',
-    layer: 0,
-    properties: {
-      fillColor: minMaxFillColor,
-      fillOpacity: minMaxFillOpacity,
-      strokeWidth: 0.0000001,
-      strokeColor: '#FFF',
-      position: minMax
+  if (q2StrokeColor && q2StrokeWidth) {
+    let q2 = [];
+    for (let i = 0; i < data.x.length; i++) {
+      q2.push({
+        x: data.x[i],
+        y: data.Q2[i]
+      });
     }
-  });
+    // Temp code because polyline is not working
+    for (let i = data.x.length - 1; i >= 0; i--) {
+      q2.push({
+        x: data.x[i],
+        y: data.Q2[i]
+      });
+    }
+
+    annotations.push({
+      type: 'polygon',
+      layer: 0,
+      properties: {
+        strokeWidth: q2StrokeWidth,
+        strokeColor: q2StrokeColor,
+        position: q2
+      }
+    });
+  }
+
+  if (minMaxFillColor && minMaxFillOpacity) {
+    let minMax = [];
+    for (let i = 0; i < data.x.length; i++) {
+      minMax.push({
+        x: data.x[i],
+        y: data.min[i]
+      });
+    }
+    for (let i = data.x.length - 1; i >= 0; i--) {
+      minMax.push({
+        x: data.x[i],
+        y: data.max[i]
+      });
+    }
+
+    annotations.push({
+      type: 'polygon',
+      layer: 0,
+      properties: {
+        fillColor: minMaxFillColor,
+        fillOpacity: minMaxFillOpacity,
+        strokeWidth: 0.0000001,
+        strokeColor: '#FFF',
+        position: minMax
+      }
+    });
+  }
 
   return annotations;
 }
