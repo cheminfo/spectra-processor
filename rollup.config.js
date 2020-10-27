83cc089ebe1ef5d8f0685af7a9ad507f8a4bfa9a
Luc Patiny <luc@patiny.com>
2020-10-27 11:32:10 +0100

    fix: jcampconverter dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonJS from 'rollup-plugin-commonjs';
-
 export default {
   input: 'src/index.js',
   output: {
     format: 'cjs',
-    file: 'lib/index.js'
+    file: 'lib/index.js',
   },
-  plugins: [
-    resolve(),
-    commonJS({
-      include: 'node_modules/**'
-    })
-  ]
 };
