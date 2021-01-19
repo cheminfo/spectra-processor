207256b4ec2a73fa5d195efb560e4bac323b3305
Luc Patiny <luc@patiny.com>
2021-01-19 17:56:36 +0100

    chore: improve test matrix parser

--- src/parser/__tests__/matrix.test.js
+++ src/parser/__tests__/matrix.test.js
@@ -1,10 +1,24 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-import matrixParsaer from '../matrix';
+import matrixParser from '../matrix';
 
-test('textParser', () => {
-  const text = readFileSync(join(__dirname, 'matrix.tsv'), 'utf8');
-  matrixParsaer(text);
-  expect(true).toBe(true);
+describe('matrix', () => {
+  it('small tsv', () => {
+    const text = readFileSync(join(__dirname, 'matrix.tsv'), 'utf8');
+    const result = matrixParser(text);
+    expect(result.matrix).toHaveLength(4);
+    expect(result.matrix[0]).toHaveLength(4);
+    expect(result.ids).toHaveLength(4);
+    expect(result.x).toHaveLength(4);
+  });
+
+  it('larger tsv', () => {
+    const text = readFileSync(join(__dirname, 'matrix2.tsv'), 'utf8');
+    const result = matrixParser(text);
+    expect(result.matrix).toHaveLength(3080);
+    expect(result.matrix[0]).toHaveLength(125);
+    expect(result.ids).toHaveLength(3080);
+    expect(result.x).toHaveLength(125);
+  });
 });
