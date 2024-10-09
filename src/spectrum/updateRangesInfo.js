5319de5c8e73f2bac10f943cdd53cde7a8833339
Luc Patiny <luc@patiny.com>
2024-10-09 15:28:48 +0200

    feat: add baseline and correctedIntegral in ranges

--- src/spectrum/updateRangesInfo.js
+++ src/spectrum/updateRangesInfo.js
@@ -19,7 +19,10 @@ export function updateRangesInfo(spectrum, ranges = []) {
       fromIndex: xFindClosestIndex(spectrum.normalized.x, range.from),
       toIndex: xFindClosestIndex(spectrum.normalized.x, range.to),
     };
+    range.deltaX = spectrum.normalized.x[fromToIndex.toIndex] - spectrum.normalized.x[fromToIndex.fromIndex];
+    range.baseline = (spectrum.normalized.y[fromToIndex.toIndex] + spectrum.normalized.y[fromToIndex.fromIndex]) * range.deltaX / 2;
     range.integration = xyIntegration(spectrum.normalized, fromToIndex);
+    range.correctedIntegration = range.integration - range.baseline;
     range.maxPoint = xyMaxYPoint(spectrum.normalized, fromToIndex);
     range.minPoint = xyMinYPoint(spectrum.normalized, fromToIndex);
     range.x = spectrum.normalized.x.slice(
