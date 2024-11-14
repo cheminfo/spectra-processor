53a32e60803cbb52a573c6be790b5524b69d5733
Luc Patiny <luc@patiny.com>
2024-11-14 15:50:05 +0100

    chore: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
 import cheminfo from 'eslint-config-cheminfo';
-import globals from 'globals';
 
 export default [
   ...cheminfo,
   {
     languageOptions: {
       globals: {
-        ...globals.node,
+        __dirname: 'readonly',
       },
     },
-    rules: {
-    }
-  }
-]
+    rules: {},
+  },
+];
